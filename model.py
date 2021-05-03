0c3bfd8d52cb9f2370c06da1461ec23791019432
Sơn Nguyễn <83537172+thanhson1604@users.noreply.github.com>
2021-05-03 17:15:39 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from tensorflow import keras
 from keras.layers import Embedding, Conv1D, MaxPool1D, GlobalAvgPool1D, GlobalMaxPool1D, Flatten, Dense
 
 vocal_size = len(word2index)
@@ -23,3 +24,28 @@ model.add(Dense(1, activation = 'sigmoid'))
 
 #Display model summary
 model.summary()
+
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+
+#Compile the model
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#Early stop training
+early_stopping = EarlyStopping(monitor = 'val_loss', patience = 10, verbose = 1)
+
+#Save the best model
+best_model = ModelCheckpoint(filepath = 'Text_Classification_bestmodel.h5', monitor = 'val_loss', verbose = 1, save_best_only = True)
+
+#Reduce learning rate
+reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.2, patience = 5, verbose = 1, min_lr = 0.001)
+
+#Start training
+model_history = model.fit(train_data, train_labels,
+                          batch_size = 512, epochs = 50, validation_split = 0.3,
+                          callbacks = [early_stopping, best_model, reduce_lr],
+                          shuffle = True)
+
+#Load the best model
+keras.models.load_model(filepath = 'Text_Classification_bestmodel.h5')
+
+model.evaluate(test_data, test_labels)
